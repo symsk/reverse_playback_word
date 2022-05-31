2f36fb5cded9e88f299d651e199d1c1d7fae06d4
symsk <k.q.t.l.0616@gmail.com>
2022-05-31 19:54:08 +0900

    Change: メソッド名を「to_gyakusaisei」に変更

--- lib/reverse_playback_word.rb
+++ lib/reverse_playback_word.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "reverse_playback_word/version"
-require "format_reverse_playback_word"
+require "format_gyakusaisei"
 
 module ReversePlaybackWord
   class Error < StandardError; end
--- spec/reverse_playback_word_spec.rb
+++ spec/reverse_playback_word_spec.rb
@@ -1,20 +1,20 @@
-require "format_reverse_playback_word"
+require "format_gyakusaisei"
 
 RSpec.describe ReversePlaybackWord do
   it "has a version number" do
     expect(ReversePlaybackWord::VERSION).not_to be nil
   end
   
-  context "「to_reverse_playback_word」が「ひらがな」に対して呼び出された時" do
+  context "「to_gyakusaisei」が「ひらがな」に対して呼び出された時" do
     it "逆再生文字に変換される" do
       string = "よろしく"
-      expect(string.to_reverse_playback_word).to eql("うきひそろゆ")
+      expect(string.to_gyakusaisei).to eql("うきひそろゆ")
     end
   end
-  context "「to_reverse_playback_word」が「ひらがな以外」に対して呼び出された時" do
+  context "「to_gyakusaisei」が「ひらがな以外」に対して呼び出された時" do
     it "そのままの文字が出力される" do
       string = "ヨロシク"
-      expect(string.to_reverse_playback_word).to eql("ヨロシク")
+      expect(string.to_gyakusaisei).to eql("ヨロシク")
     end
   end
 end
