e8e4dcc2fe81d758aa363e8d7695531d72f28ba8
symsk <k.q.t.l.0616@gmail.com>
2022-05-31 14:51:40 +0900

    Add: メソッドの記述を追加

--- lib/reverse_playback_word.rb
+++ lib/reverse_playback_word.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "reverse_playback_word/version"
+require "format_reverse_playback_word"
 
 module ReversePlaybackWord
   class Error < StandardError; end
-  # Your code goes here...
 end
